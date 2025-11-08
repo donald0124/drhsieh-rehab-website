87327d288e4a13f3eba361df431610d16e7332cb
Shih-po Hsieh <donald0124@gmail.com>
2025-11-08 09:52:44 +0800

    feat: Add Astro config and deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // 填入您在 Cloudflare 購買的 "自訂網域"
+  site: 'https://drhsieh-rehab.com',
+
+  // 網站是放在根目錄 (不是 /blog/ 之類的子資料夾)
+  base: '/',
+});
